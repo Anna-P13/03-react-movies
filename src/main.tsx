a23e400de535ea413cf5509115457c4438d214b3
Anna Pastykhova <rainbow261@gmail.com>
2025-08-08 23:01:04 +0300

    2

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./components/App/App.tsx";
+import App from "./components/App/App";
 
-createRoot(document.getElementById("root") as HTMLDivElement).render(
-  <StrictMode></StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
